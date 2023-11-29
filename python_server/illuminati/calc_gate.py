9cda958dbbda3fec8797d47ea4f7d204c0dec0c6
levicorp-us <112557732+levicorp-us@users.noreply.github.com>
2023-11-30 00:23:30 +0900

    modified calc gate

--- python_server/illuminati/calc_gate.py
+++ python_server/illuminati/calc_gate.py
@@ -1,4 +1,4 @@
-import qiskit
+from qiskit import QuantumCircuit
 
 applied_gates = {
 	0:[],
@@ -13,67 +13,74 @@ applied_gates = {
 	9:[]
 }
 
-lamp_state = [ # |0> prob, |1> prob, is_observed
-	[1,0, False],
-	[0,1, False],
-	[1,0, False],
-	[0,1, False],
-	[1,0, False],
-	[0,1, False],
-	[1,0, False],
-	[0,1, False],
-	[1,0, False],
-]
-
-def game_end():
+def calc_gate():
+    qc = QuantumCircuit(9,9)
+    
+    # initialize circuit
+    for i in range(9):
+        if i % 2 != 0:
+            qc.x(i)
+    
+    
     for i in range(len(applied_gates[0])):
-        
         gates = get_gates_at_idx(i)
-        
-        # check if there are any effective gates
+
+        # Check if there are any effective gates
         effective_gates = []
-        for gate in gates: 
-            if gate != "Identity" and gate != "Conditional" and gate != "NotConditional" and gate != "Empty":
-                effective_gates = [gate, gates.find(gate)]
-        
-        # pass if there's none. this includes smth useless like conditional identity
-        if effective_gates.is_empty():
+        for gate in gates:
+            if gate not in ["Identity", "Conditional", "NotConditional", "Empty"]:
+                effective_gates = [gate, gates.index(gate)]
+
+        # Pass if there's none (includes something useless like conditional identity)
+        if not effective_gates:
+            for idx in range(9): qc.i(idx) # 
             continue
-        # pass if the target lamp is already observed
-        if lamp_state[effective_gates[0][1]][2]:
-            continue
-        
-        # swap
-        if effective_gates[0][0] == "Swap":
-            swap_states(effective_gates)
-            continue
-        
-        # observe
-        if effective_gates[0][0] == "Observe":
-            lamp_state[effective_gates[0][1]][2] = True # mark the lamp as observed
-            continue
-        
-        # check if conditional/not_conditional in gates and save its idx
+
+        # Check if conditional/not_conditional in gates and save its idx
         cond_idx = -1
         not_conditional = False
         if "Conditional" in gates:
-            cond_idx = gates.find("Conditional")
+            cond_idx = gates.index("Conditional")
         elif "NotConditional" in gates:
-            cond_idx = gates.find("NotConditional")
+            cond_idx = gates.index("NotConditional")
             not_conditional = True
-        
-        # apply the gate
-        match effective_gates[0][0]:
-            case "Hadamard":
-                pass
+            
+        gate_name = effective_gates[0][0]
+        gate_idx = effective_gates[0][1]
+
+        match gate_name:
+            case "Swap":
+                src_idx =gate_idx
+                dst_idx = effective_gates[1][1]
+                qc.swap(src_idx, dst_idx)
+                continue
+            case "Observe":
+                qc.measure(gate_idx, gate_idx)
+                continue
             case "Not":
-                pass
-			
-def swap_states(gate_info):
-	pass
-		
-def get_gates_at_idx(idx): # any way to access by columns in godot?
-	gates = []
-	for array in applied_gates.values():
-		gates.append(array[idx])
-	return gates
+                if cond_idx != -1:
+                    if not_conditional:
+                        qc.x(cond_idx)
+                        qc.cx(cond_idx, gate_idx)
+                        qc.x(cond_idx)
+                    else:
+                        qc.cx(cond_idx, gate_idx)
+                else:
+                    qc.x(gate_idx)
+            case "Hadamard":
+                if cond_idx != -1:
+                    if not_conditional:
+                        qc.x(cond_idx)
+                        qc.ch(cond_idx, gate_idx)
+                        qc.x(cond_idx)
+                    else:
+                        qc.ch(cond_idx, gate_idx)
+                else:
+                    qc.h(gate_idx)
+
+
+def get_gates_at_idx(idx):  # Any way to access by columns in Python?
+    gates = []
+    for array in applied_gates.values():
+        gates.append(array[idx])
+    return gates
